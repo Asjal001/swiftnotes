daf707de72a747f2b89433863342fdb5faefaa6b
Asjal001 <142677498+Asjal001@users.noreply.github.com>
2026-08-07 18:52:27 +0500

    implement notes API

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import pino from 'pino'
 import pinoHttp from 'pino-http';
 import authRoutes from './routes/authRoutes.js'
-
+import noteRoutes from './routes/noteRoutes.js'
 dotenv.config();
 const isDev = process.env.NODE_ENV !== 'production'
 const PORT = process.env.PORT || 5000;
@@ -16,6 +16,7 @@ app.use(pinoHttp({logger}));
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth',authRoutes);
+app.use('/api/notes',noteRoutes);
 app.get('/api/health', (req, res)=>{
   res.sendStatus(200);
 });
